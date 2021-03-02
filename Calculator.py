bf15b7d51b3c7c445aa616f508eb129e0919a76f
Werick Nalyson <wericknalyson@gmail.com>
2021-03-02 16:05:50 -0300

    add window config

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget
 
 
 class Calculator(QMainWindow):
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent=None):
         super().__init__(parent)
+        self.setWindowTitle('Calculadora')
+        self.setFixedSize(400, 400)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+        
+
+        self.setCentralWidget(self.cw)
 
 
 if __name__ == "__main__":
